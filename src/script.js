488adc7fbbbe7a20cbb3f406e0f1b4a9fe71ae83
Mohammed Faais <mohammedfaais67@gmail.com>
2022-01-19 15:05:57 +0530

    Hamburger

--- src/script.js
+++ src/script.js
@@ -1,5 +1,15 @@
 const hamburger = document.querySelector('#hamburger');
 const navsub = document.querySelector('#nav-list');
+const closeIcon = document.querySelector('#closeIcon')
+const openIcon = document.querySelector('#openIcon')
+const hamburgerLine =  document.querySelector('.line')
 hamburger.addEventListener('click', () => {
- navsub.classList.toggle('show');
+ navsub.classList.toggle('show')
+ hamburger.classList.toggle('open')
+//  closeIcon.style.display='block';
+//  openIcon.style.display='none';
+//  closeIcon.classList.add('view');
+//  openIcon.classList.add('hide');
 });
+
+
