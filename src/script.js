75cc6ac8e2d1c031c17f68957eeda18df5659af5
Faais <81349206+mohammedfaais@users.noreply.github.com>
2024-07-03 09:12:10 +0000

    Nav link items updated

--- src/script.js
+++ src/script.js
@@ -1,15 +1,14 @@
-const hamburger = document.querySelector('#hamburger');
-const navsub = document.querySelector('#nav-list');
-const closeIcon = document.querySelector('#closeIcon')
-const openIcon = document.querySelector('#openIcon')
-const hamburgerLine =  document.querySelector('.line')
-hamburger.addEventListener('click', () => {
- navsub.classList.toggle('show')
- hamburger.classList.toggle('open')
-//  closeIcon.style.display='block';
-//  openIcon.style.display='none';
-//  closeIcon.classList.add('view');
-//  openIcon.classList.add('hide');
+const hamburger = document.querySelector("#hamburger");
+const navsub = document.querySelector("#nav-list");
+const closeIcon = document.querySelector("#closeIcon");
+const openIcon = document.querySelector("#openIcon");
+const hamburgerLine = document.querySelector(".line");
+hamburger.addEventListener("click", () => {
+  navsub.classList.toggle("show");
+  hamburger.classList.toggle("open");
+  navsub.classList.toggle("active");
+  //  closeIcon.style.display='block';
+  //  openIcon.style.display='none';
+  //  closeIcon.classList.add('view');
+  //  openIcon.classList.add('hide');
 });
-
-
